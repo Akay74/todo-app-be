088642c752f7eb35314890de0544e74c9a45136e
Akay <akaudeh@gmail.com>
2023-10-30 11:43:04 +0100

    ✨ add routes and controllers for updateToDo and deleteToDo

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -2,11 +2,15 @@ const { Router } = require("express");
 const {
   getToDos,
   saveToDo,
+  updateToDo,
+  deleteToDo
 } = require("../controller/ToDoController");
 
 const router = Router();
 
 router.get("/get", getToDos);
 router.post("/save", saveToDo);
+router.post("/save", updateToDo);
+router.post("/save", deleteToDo);
 
 module.exports = router;
